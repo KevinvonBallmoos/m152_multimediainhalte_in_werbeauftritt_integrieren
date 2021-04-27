731ce97abd4941fec470727ecffdd80e2f1e00cc
KevinvonBallmoos <kevin.vonballmoos@ict.csbe.ch>
2021-04-27 19:52:10 +0200

    added websocket

--- websocket.js
+++ websocket.js
@@ -1,24 +1,43 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var WebSocket = require("ws");
-var express = require('express');
-var port = process.env.PORT || 8080;
-var server = express()
-    .listen(port, function () { return console.log("Listening on " + port); });
-var wss = new WebSocket.Server({ server: server });
+var ws = require('ws');
+var wss = new WebSocket.Server({ port: 8080 });
+function heartbeat() {
+    ws.isAlive = true;
+}
 wss.on('connection', function (client) {
-    client.on('message', function (data) {
-        client.send(data);
-        // for (let client1 of Array.from(wss.clients)) {
-        //     if (client1 !== client) {
-        //         client1.send(data);
-        //     }
-        // }
-        console.log(data);
-        Array.from(wss.clients)
-            .filter(function (connectedClient) { return connectedClient !== client; })
-            .forEach(function (connectedClient) { return connectedClient.send(data); });
+    client.on('pong', function () {
+        ws.isAlive = true;
+        console.log("here");
     });
+    client.on('message', function (data) {
+        for (var _i = 0, _a = Array.from(wss.clients); _i < _a.length; _i++) {
+            var client2 = _a[_i];
+            if (client2 !== client) {
+                client2.send(data);
+            }
+        }
+        /* Array.from(wss.clients)
+             .filter(client1 => client1 !== client)
+             .forEach(client1 => client1.send(data));*/
+    });
+    setInterval(function ping() {
+        wss.clients.forEach(function each(ws) {
+            var extWs = ws;
+            extWs.on('pong', function () {
+                console.log(extWs.isAlive);
+            });
+            if (!extWs.isAlive) {
+                console.log("Exited");
+                ws.terminate();
+            }
+            extWs.ping("test", false, function (err) {
+                console.log(err);
+            });
+            extWs.isAlive = false;
+        });
+    }, 2000);
     client.send('Welcome in the Chat Corner!');
 });
 //# sourceMappingURL=websocket.js.map
--- websocket.ts
+++ websocket.ts
@@ -1,33 +1,53 @@
 import * as WebSocket from "ws";
 
-const express = require('express');
+const ws = require('ws');
 
-const port = process.env.PORT || 8080;
+const wss = new WebSocket.Server({port: 8080});
 
-const server = express()
-    .listen(port, () => console.log(`Listening on ${port}`));
+interface ExtWebSocket extends WebSocket {
+    isAlive: boolean;
+}
 
-
-const wss = new WebSocket.Server({ server });
-
+function heartbeat() {
+    ws.isAlive = true;
+}
 
 wss.on('connection', (client: WebSocket) => {
 
-    client.on('message', data => {
-
-        client.send(data);
-        // for (let client1 of Array.from(wss.clients)) {
-        //     if (client1 !== client) {
-        //         client1.send(data);
-        //     }
-        // }
-        console.log(data);
-        Array.from(wss.clients)
-            .filter(connectedClient => connectedClient !== client)
-            .forEach(connectedClient => connectedClient.send(data));
-
+    client.on('pong', () => {
+        ws.isAlive = true
+        console.log("here");
     });
 
-    client.send('Welcome in the Chat Corner!');
-});
+    client.on('message', data => {
+        for (let client2 of Array.from(wss.clients)) {
+            if (client2 !== client) {
+                client2.send(data);
+            }
+        }
 
+        /* Array.from(wss.clients)
+             .filter(client1 => client1 !== client)
+             .forEach(client1 => client1.send(data));*/
+    });
+
+    setInterval(function ping() {
+        wss.clients.forEach(function each(ws) {
+            const extWs = ws as ExtWebSocket;
+            extWs.on('pong', () => {
+                console.log(extWs.isAlive);
+            })
+            if (!extWs.isAlive) {
+                console.log("Exited");
+                ws.terminate();
+            }
+            extWs.ping("test", false, err => {
+                console.log(err);
+            });
+            extWs.isAlive = false;
+        });
+    }, 2000);
+
+
+    client.send('Welcome in the Chat Corner!')
+});
